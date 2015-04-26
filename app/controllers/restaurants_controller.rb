f10c3908e243960ebbafc8b30259e1ff7181c030
Daiani <Daianim3@Daianis-MacBook-Air.local>
2015-04-26 19:55:59 -0400

    error in restaurant index still

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,35 @@ class RestaurantsController < ApplicationController
   	   @restaurants = Restaurant.all
   end
 
+  def
+
   def show
   	   @restaurants = Restaurant.find(params[:id])
   end
+
+  def new
+  	@restaurants = Restaurants.new
+  end
+
+  def create
+  	@restaurants = Restaurants.new(restaurants_params)
+
+  	if @restaurants.save
+  		redirect_to restaurants_url
+  	else
+  		render :new
+
+  	end
+
+  end
+  private
+  def restaurants_params
+  	params.require(:name).permit(:name, :category, :price_range, :address)
+
+  end
 end
+
+
+
+
+
